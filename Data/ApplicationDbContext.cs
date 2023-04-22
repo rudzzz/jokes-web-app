2c302481cf51ad2bb080665f0fac06b7aeb8640e
rudzzz <rudsolza13@gmail.com>
2023-04-22 11:51:27 -0300

    code

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Jokes.Models;
+
+namespace Jokes.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Jokes.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ Migrations/20230422133630_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Xml.Linq;
+
+#nullable disable
+
+namespace Jokes.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.CreateTable(
+            name: "Jokes",
+            columns: table => new
+            {
+                Id = table.Column<int>(nullable: false)
+                    .Annotation("Sq1Server:Identity", "1, 1"),
+                JokeQuestion = table.Column<string>(nullable: true),
+                JokeAnswer = table.Column<string>(nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Jokes", x => x.Id);
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
